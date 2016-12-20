529a639c541ff8e8e88e3c636311a2449ef89016
Marco Jonkers <contact@marcojonkers.com>
2016-12-20 20:04:09 +0100

    Add TODO to generate_header.bat

--- generate_header.bat
+++ generate_header.bat
@@ -4,5 +4,7 @@ set var=%PATH%;%JAVA_HOME%\bin
 IF ERRORLEVEL 1 (
     set "PATH=%var%"
 )
-:: Would be nice to get the path to the forgeSrc jar file somehow
+:: TODO: Get path to forgeSrc jar dynamically
+:: otherwise this file is useless to the public
+:: Note: loader_jni.h is in version control
 javah -o loader_jni.h -classpath build\classes\production\mcraytracer_main;C:\Users\Marco\.gradle\caches\minecraft\net\minecraftforge\forge\1.10.2-12.18.2.2099\snapshot\20160518\forgeSrc-1.10.2-12.18.2.2099.jar com.marcojonkers.mcraytracer.Raytracer
