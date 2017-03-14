9dfc84ece0d9df1f1e52e61ac81e2f0a4c418bb9
Marco Jonkers <contact@marcojonkers.com>
2017-03-14 16:23:17 +0100

    Change CppVertexBuffer to pass data to C++

--- src/main/java/com/marcojonkers/mcraytracer/CppVertexBuffer.java
+++ src/main/java/com/marcojonkers/mcraytracer/CppVertexBuffer.java
@@ -1,7 +1,5 @@
 package com.marcojonkers.mcraytracer;
 
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.vertex.VertexFormat;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -10,39 +8,30 @@ import java.nio.ByteBuffer;
 
 @SideOnly(Side.CLIENT)
 public class CppVertexBuffer {
-    private int glBufferId;
-    private final VertexFormat vertexFormat;
-    private int count;
+    VertexFormat format;
 
     public CppVertexBuffer(VertexFormat vertexFormatIn) {
-        this.vertexFormat = vertexFormatIn;
-        this.glBufferId = OpenGlHelper.glGenBuffers();
+        this.format = vertexFormatIn;
     }
 
     public void bindBuffer() {
-        OpenGlHelper.glBindBuffer(OpenGlHelper.GL_ARRAY_BUFFER, this.glBufferId);
+        // Do nothing
     }
 
     public void bufferData(ByteBuffer data) {
-        this.bindBuffer();
-        OpenGlHelper.glBufferData(OpenGlHelper.GL_ARRAY_BUFFER, data, 35044);
-        this.unbindBuffer();
-        this.count = data.limit() / this.vertexFormat.getNextOffset();
+        // Pass to C++
+        Raytracer.getRaytracer().setVertexBuffer(data, data.remaining());
     }
 
     public void drawArrays(int mode) {
-        GlStateManager.glDrawArrays(mode, 0, this.count);
+        // Do nothing
     }
 
     public void unbindBuffer() {
-        OpenGlHelper.glBindBuffer(OpenGlHelper.GL_ARRAY_BUFFER, 0);
+        // Do nothing
     }
 
     public void deleteGlBuffers() {
-        if (this.glBufferId >= 0)
-        {
-            OpenGlHelper.glDeleteBuffers(this.glBufferId);
-            this.glBufferId = -1;
-        }
+
     }
 }
