d2f7ce8fdf2ad788500cb25715b66afcc668b42f
Marco Jonkers <contact@marcojonkers.com>
2017-03-14 14:15:54 +0100

    Make CppVertexBuffer identical to VertexBuffer

--- src/main/java/com/marcojonkers/mcraytracer/CppVertexBuffer.java
+++ src/main/java/com/marcojonkers/mcraytracer/CppVertexBuffer.java
@@ -1,5 +1,7 @@
 package com.marcojonkers.mcraytracer;
 
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.vertex.VertexFormat;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -8,28 +10,39 @@ import java.nio.ByteBuffer;
 
 @SideOnly(Side.CLIENT)
 public class CppVertexBuffer {
+    private int glBufferId;
+    private final VertexFormat vertexFormat;
+    private int count;
 
     public CppVertexBuffer(VertexFormat vertexFormatIn) {
-
+        this.vertexFormat = vertexFormatIn;
+        this.glBufferId = OpenGlHelper.glGenBuffers();
     }
 
     public void bindBuffer() {
-
+        OpenGlHelper.glBindBuffer(OpenGlHelper.GL_ARRAY_BUFFER, this.glBufferId);
     }
 
     public void bufferData(ByteBuffer data) {
-
-    }
-
-    public void deleteGlBuffers() {
-
+        this.bindBuffer();
+        OpenGlHelper.glBufferData(OpenGlHelper.GL_ARRAY_BUFFER, data, 35044);
+        this.unbindBuffer();
+        this.count = data.limit() / this.vertexFormat.getNextOffset();
     }
 
     public void drawArrays(int mode) {
-
+        GlStateManager.glDrawArrays(mode, 0, this.count);
     }
 
     public void unbindBuffer() {
+        OpenGlHelper.glBindBuffer(OpenGlHelper.GL_ARRAY_BUFFER, 0);
+    }
 
+    public void deleteGlBuffers() {
+        if (this.glBufferId >= 0)
+        {
+            OpenGlHelper.glDeleteBuffers(this.glBufferId);
+            this.glBufferId = -1;
+        }
     }
 }
